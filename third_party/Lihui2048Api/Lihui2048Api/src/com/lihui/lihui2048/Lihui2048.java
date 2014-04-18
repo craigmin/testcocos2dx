6bd9e078c1fd860eb98b316fd71fd7be54e77a25
craig <craig.min@cssinco.cn>
2014-04-18 12:15:16 +0800

    refactor

--- third_party/Lihui2048Api/Lihui2048Api/src/com/lihui/lihui2048/Lihui2048.java
+++ third_party/Lihui2048Api/Lihui2048Api/src/com/lihui/lihui2048/Lihui2048.java
@@ -1,10 +1,14 @@
 package com.lihui.lihui2048;
 
-import android.util.Log;
-import cn.lihui.game.MatrixOperator;
+import cn.lihui.game.object.Matrix;
 
 public class Lihui2048 {
 	private static Lihui2048 instanse;
+	private Matrix matrix;
+
+	Lihui2048() {
+		matrix = new Matrix();
+	}
 
 	public static Lihui2048 getInstance() {
 		if (instanse == null) {
@@ -15,44 +19,42 @@ public class Lihui2048 {
 	}
 
 	public boolean isMovable() {
-		return MatrixOperator.getInstance().isMovable();
+		return matrix.isMovable();
 	}
 
 	public void move(int type) {
-		MatrixOperator.getInstance().move(type);
+		matrix.move(type);
 	}
 
 	public int getCurrentScore() {
-		return MatrixOperator.getInstance().getCurrentScore();
+		return matrix.getScore().getCurrent();
 	}
 
 	public int getBestScore() {
-		return MatrixOperator.getInstance().getBestScore();
+		return matrix.getScore().getBest();
 	}
 
 	public boolean isWin() {
-		return MatrixOperator.getInstance().isWin();
+		return matrix.isPointReachMax();
 	}
 
 	public int getValue(int x, int y) {
-		return MatrixOperator.getInstance().getValue(x, y);
+		return matrix.getPointValue(x, y);
 	}
 
 	public void reset() {
-		MatrixOperator.getInstance().reset();
+		matrix.reset();
 	}
 
 	public int getAnim(int x, int y) {
-		return MatrixOperator.getInstance().getAnim(x, y);
+		return matrix.getPointAnim(x, y);
 	}
 
-	public void cleanRect(int x, int y) {
-		Log.i("", "cleanRect: " + x + " " + y);
-		MatrixOperator.getInstance().cleanPoint(x, y);
+	public void cleanPoint(int x, int y) {
+		matrix.cleanPointValue(x, y);
 	}
 
 	public void reArrange() {
-		MatrixOperator.getInstance().rearrangeMatrix();
+		matrix.rearrangeMatrix();
 	}
-
 }
