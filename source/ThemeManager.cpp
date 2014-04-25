859ac64a5173b7d82c702d5f241a49dcd51791e6
10kgrat <stone.shi@cssinco.cn>
2014-04-25 15:50:23 +0800

    theme update

--- source/ThemeManager.cpp
+++ source/ThemeManager.cpp
@@ -39,7 +39,7 @@ const char* ThemeManager::getFontName() {
 	case themePlain:
 		return "arial";
 	case themeCandy:
-		return "candy";
+		return "arial";
 	default:
 		//sprintf(buff,"themePlain/");
 		break;
